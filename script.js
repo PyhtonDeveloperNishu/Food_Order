3d29bdd43ecbba5e46a5ca9dbece36d4315964ef
PyhtonDeveloperNishu <yashkumar5147@gmail.com>
2025-03-07 07:05:55 +0530

    loading part added

--- script.js
+++ script.js
@@ -4,34 +4,43 @@ let burger = document.querySelector("#burger");
 let fries = document.querySelector("#fries"); 
 let body = document.querySelector("body")
 
-
+// when there is no order click in image_con
 let  noOrder = document.createElement("h1");
 noOrder.classList.add("no-order");
 noOrder.innerText="See Your Order Here"
 image_con.prepend(noOrder);
+
+// This is for show order id 
 let orderId = document.createElement("h3");
 orderId.classList.add("orderId")
+
+// this is for loading elment
+let loader = document.createElement("div");
+loader.classList.add("loader");
+
+
+// click order btn and show your order and id number 
 btn.addEventListener("click", () => {
     noOrder.innerHTML="";
     image_con.innerHTML = "";
-    orderId.innerHTML=""
-    
-    // orderId.innerHTML="";
-    let randomTime = Math.floor(Math.random() * 1000);
+    orderId.innerHTML=""  
+    if(burger.checked || fries.checked || drink.checked)  {
+        loader.style.display="block" // show loader
+        image_con.append(loader);
+    }
+    let randomTime = Math.floor(Math.random() * 3000);
     const myPromise = new Promise((resolve, reject) => { 
         setTimeout(() => {
-            resolve({ message: "success", time: randomTime });
+            loader.style.display="none";  // hide loader
+            resolve()
         }, randomTime);
-    });
-
-    myPromise.then((response) => {
-       // console.log(response); // Logs success message and time
-       
+    });    
+    myPromise.then((response) => {       // console.log(response); // Logs success message an time
         let image = document.createElement("img");
-        
         image.classList.add("img_con");
-        
+        // give order id number on UI
         orderId.innerText=`Order ID : ${randomTime}`;
+
         if (burger.checked) { 
             image.src = "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2VyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60";
         }else if(fries.checked){
